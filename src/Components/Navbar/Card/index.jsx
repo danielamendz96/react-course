2aa9a03bf998e5f49ca421326927656df815816d
Daniela Mendoza <danielamendoz96@gmail.com>
2023-12-14 08:44:49 -0400

    consumiendo una fake api para la tienda virtual

--- src/Components/Navbar/Card/index.jsx
+++ src/Components/Navbar/Card/index.jsx
@@ -1,18 +1,21 @@
 
 
-const Card = () => {
+const Card = (data) => {
     return(
-        <div className='bg-white cursor-pointer w-56 h-60'>
+        <div className='bg-white cursor-pointer w-56 h-60 mt-4 rounded-lg border-solid border-2 border-gray-500'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Clothes</span>
-                    <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/449977/pexels-photo-449977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="jacket" />
-                    <button className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
+                <span className='absolute bottom-0 left-0 bg-white/70 rounded-lg
+                 text-black -xs m-2 px-3 py-0.5'>{data.data.category}</span>
+                    <img className='w-full h-full mt-2 object-contain rounded-lg' src={data.data.image}                    alt={data.data.title}/>
+                    <button className='absolute top-0 right-0 flex justify-center items-center
+                     bg-red-200 w-6 h-6 rounded-full m-2 p-1 text-red-900'>
                         +
                         </button>
             </figure>
-            <p className='flex justify-between'>
-                <span className='text-sm font-light'>Jacket</span>
-                <span className='text-lg font-medium'>$60</span>
+
+            <p className='flex justify-between truncate mr-2 ml-2'>
+                <span className='text-sm font-light truncate'>{data.data.title}</span>
+                <span className='text-m font-medium'>${data.data.price}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Navbar/Layout";
 import Card from "../../Components/Navbar/Card";
 
+
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() =>{
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => setItems(data))
+
+  }, [] )
+
     return (
       <Layout>
           Home
-          <Card />
+          <div className='grid gap-4 grid-cols-4 w-full p-5 max-w-screen-lg'>
+          {
+            items?.map(item  => (
+              <Card key={item.id} data={item} />
+            ))
+          }
+
+          </div>
+          
       </Layout>
         
    
